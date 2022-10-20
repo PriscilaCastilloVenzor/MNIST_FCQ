7fb20b5af677f7af33883fc42b3cad5d10f55470
PriscilaCastilloVenzor <102831942+PriscilaCastilloVenzor@users.noreply.github.com>
2022-10-20 08:51:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ st.sidebar.header("Esta aplicación es solo un demo")
 st.sidebar.image("http://fcq.uach.mx/images/institucionales/Escudos/Dorado.png")
 boton = st.button("globos")
 if boton:
-  st.balloon()
+  st.balloons()
       
